e94f72c58ea3da084dfb564bc0a94b649eebd473
Viktor X <x.viktor.13@gmail.com>
2021-05-08 00:37:34 +0700

    Edit users tests

--- users/tests/api/test_users.py
+++ users/tests/api/test_users.py
@@ -5,20 +5,20 @@ from mixer.backend.django import mixer
 pytestmark = [pytest.mark.django_db]
 
 
-def test_get_user_list(client):
+def test_get_user_list(api_client):
     """Получение списка пользователей."""
     url = reverse('users')
-    response = client.get(url)
+    response = api_client.get(url)
 
     assert response.status_code == 200
 
 
-def test_new_user_in_list(client):
+def test_new_user_in_list(api_client):
     """Появление нового пользователя."""
     url = reverse('users')
-    response = client.get(url)
+    response = api_client.get(url)
 
     mixer.blend('users.User')
-    new_response = client.get(url)
+    new_response = api_client.get(url)
 
     assert response.content != new_response.content
