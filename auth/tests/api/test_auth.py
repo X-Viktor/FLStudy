46c6cd27fa6a77a71831b61f8008a2d437b48883
Viktor X <x.viktor.13@gmail.com>
2021-05-08 00:17:50 +0700

    Edit auth tests

--- auth/tests/api/test_auth.py
+++ auth/tests/api/test_auth.py
@@ -3,8 +3,25 @@ import pytest
 pytestmark = [pytest.mark.django_db]
 
 
-def test_get_token(client, new_user_api):
-    """Получение JWT."""
-    response = client.post('/api/auth/jwt/create/', data=new_user_api)
+def test_user_registration(api_client):
+    """Регистрация пользователя через API."""
+    data = {
+        'username': 'test1',
+        'email': 'test1@test.ru',
+        'password': 'test1234',
+    }
+    response = api_client.post('/api/auth/users/', data=data)
+    assert response.status_code == 201
 
+
+def test_get_token(api_client, user):
+    """Получение JWT."""
+    url = '/api/auth/jwt/create/'
+    login_data = {
+        'username': user.username,
+        'password': 'test1234',
+    }
+    response = api_client.post(url, data=login_data)
+
+    assert response.data['access'] is not None
     assert response.status_code == 200
---
+++ conftest.py
@@ -0,0 +1,38 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = [pytest.mark.django_db]
+
+
+@mixer.middleware('users.User')
+def encrypt_password(user):
+    """
+    Кэширует пароль 'test1234' для всех пользователей,
+    созданных во время тестирования.
+    """
+    user.set_password('test1234')
+    return user
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
+@pytest.fixture
+def token(api_client, task):
+    """Веб-токен JSON."""
+    url = '/api/auth/jwt/create/'
+    login_data = {
+        'username': task.customer.username,
+        'password': 'test1234',
+    }
+    token = api_client.post(url, login_data)
+    return token
+
+
+@pytest.fixture
+def user(api_client):
+    """Пользователь."""
+    return mixer.blend('users.User')
